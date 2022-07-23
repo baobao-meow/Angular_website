3e0c1fd838eca37c004990cd943f5aadeb3e819b
baobao-meow <g.bao@wustl.edu>
2022-07-22 22:39:13 -0500

    contact

--- buy-and-sell/src/app/contact-page/contact-page.component.ts
+++ buy-and-sell/src/app/contact-page/contact-page.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute, Router } from '@angular/router';
+import { Listing } from '../types';
+import { fakeListings } from '../fake-data';
 @Component({
   selector: 'app-contact-page',
   templateUrl: './contact-page.component.html',
   styleUrls: ['./contact-page.component.css']
 })
 export class ContactPageComponent implements OnInit {
+  email: String = '';
+  message: String = '';
+  listing: Listing;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    ) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.listing = fakeListings.find(listing => listing.id === id)!;
+    this.message = 'Hi, I am interested in your ${this.listing.name.toLowerCase()}!'
   }
+    sendMessage(): void {
+      alert('your mesage has been sent');
+      this.router.navigateByUrl('/listings');
+    }
+
 
 }
