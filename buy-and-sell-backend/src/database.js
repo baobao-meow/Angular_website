7bc0650b530422449851def0cd3f5ca2c59e65d1
baobao-meow <g.bao@wustl.edu>
2022-07-24 19:29:32 -0500

    server

---
+++ buy-and-sell-backend/src/database.js
@@ -0,0 +1,25 @@
+
+import mysql from 'mysql';
+
+let connection;
+
+export const db = {
+    connect: () => {
+        connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'hapi-server',
+            password: 'abc123!',
+            database: 'buy-and-sell',
+            //socketPath: process.env.DB_SOCKET,
+        });
+        connection.connect();
+    },
+    query: (queryString, escapedValues) =>
+        new Promise((resolve, reject) => {
+            connection.query(queryString, escapedValues, (error, results, fields) => {
+                if (error) reject(error);
+                resolve({ results, fields });
+            })
+        }),
+    end: () => connection.end(),
+}
