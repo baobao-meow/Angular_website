d26778fee4d3ca88b5048ade9e7cb6a23593f897
baobao-meow <g.bao@wustl.edu>
2022-07-26 15:33:24 -0500

    backend finish

--- buy-and-sell-backend/src/routes/index.js
+++ buy-and-sell-backend/src/routes/index.js
@@ -2,9 +2,17 @@ import { addViewToListingRoute } from './addViewToListing';
 import { getAllListingsRoute } from './getAllListings';
 import { getListingRoute } from './getListing';
 import { getUserListingsRoute } from './getUserListings.js';
+import { createNewListingRoute } from './createNewListing';
+import { deleteListingRoute } from './deleteListing';
+import { updateListingRoute } from './updateListing';
+
+
 export default [
     addViewToListingRoute,
     getAllListingsRoute,
     getListingRoute,
     getUserListingsRoute,
+    createNewListingRoute,
+    updateListingRoute,
+    deleteListingRoute,
 ];
