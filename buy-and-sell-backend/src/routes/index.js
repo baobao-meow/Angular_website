939e7ebbf6255dffa799914ea607ff9f925a559a
baobao-meow <g.bao@wustl.edu>
2022-07-26 14:36:25 -0500

    getuserlisting

--- buy-and-sell-backend/src/routes/index.js
+++ buy-and-sell-backend/src/routes/index.js
@@ -1,9 +1,10 @@
 import { addViewToListingRoute } from './addViewToListing';
 import { getAllListingsRoute } from './getAllListings';
 import { getListingRoute } from './getListing';
-
+import { getUserListingsRoute } from './getuSERlISTINGS.JS';
 export default [
     addViewToListingRoute,
     getAllListingsRoute,
     getListingRoute,
+    getUserListingsRoute,
 ];
