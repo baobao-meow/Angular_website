42eeb0ea3c23c1cf84b2d5b890a3a97e2e45c0c1
baobao-meow <g.bao@wustl.edu>
2022-07-26 21:13:37 -0500

    update

--- buy-and-sell/src/environments/environment.ts
+++ buy-and-sell/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDOXiPITsTa32mi514WRkYazQCqK6Kb1EY",
+    authDomain: "buy-and-sell-db603.firebaseapp.com",
+    projectId: "buy-and-sell-db603",
+    storageBucket: "buy-and-sell-db603.appspot.com",
+    messagingSenderId: "775720638365",
+    appId: "1:775720638365:web:8e87a6000d2ccc3165c174",
+    measurementId: "G-TBRVSTJL1J"
+  }
 };
 
 /*
