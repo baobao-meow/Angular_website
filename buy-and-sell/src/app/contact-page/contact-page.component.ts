2dca098e7fbe999eed9dc78f24f5d4cc35f1ebe8
baobao-meow <g.bao@wustl.edu>
2022-07-26 19:17:31 -0500

    new

--- buy-and-sell/src/app/contact-page/contact-page.component.ts
+++ buy-and-sell/src/app/contact-page/contact-page.component.ts
@@ -1,31 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Listing } from '../types';
-import { fakeListings } from '../fake-data';
+import { ListingsService } from '../listings.service';
+
+
 @Component({
   selector: 'app-contact-page',
   templateUrl: './contact-page.component.html',
   styleUrls: ['./contact-page.component.css']
 })
 export class ContactPageComponent implements OnInit {
-  email: String = '';
-  message: String = '';
+  email: string = '';
+  message: string = '';
   listing: Listing;
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    ) { }
+    private listingsService: ListingsService,
+  ) { }
 
   ngOnInit(): void {
-    const id = this.route.snapshot.paramMap.get('id');
-    this.listing = fakeListings.find(listing => listing.id === id)!;
-    this.message = 'Hi, I am interested in your ${this.listing.name.toLowerCase()}!'
+    const id = this.route.snapshot.paramMap.get('id')!;
+    this.listingsService.getListingById(id)
+      .subscribe(listing => {
+        this.listing = listing;
+        this.message = `Hi, I'm interested in your ${this.listing.name.toLowerCase()}!`;
+      });
   }
-    sendMessage(): void {
-      alert('your mesage has been sent');
-      this.router.navigateByUrl('/listings');
-    }
 
-
+  sendMessage(): void {
+    alert('Your message has been sent!');
+    this.router.navigateByUrl('/listings');
+  }
 }
