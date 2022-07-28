4bb5072390eee3d0c7b95e5e914dd573813317d4
baobao-meow <g.bao@wustl.edu>
2022-07-27 20:13:47 -0500

    auth

--- buy-and-sell/src/app/listings.service.ts
+++ buy-and-sell/src/app/listings.service.ts
@@ -62,8 +62,16 @@ export class ListingsService {
       })
     })
   }
+  
   deleteListing(id: string): Observable<any> {
-    return this.http.delete(`/api/listings/${id}`);
+    return new Observable<any>(observer => {
+      this.auth.user.subscribe(user => {
+        user && user.getIdToken().then(token => {
+          this.http.delete(`/api/listings/${id}`, httpOptionsWithAuthToken(token))
+            .subscribe(() => observer.next());
+        })
+      })
+    })
   }
 
   createListing(name: string, description: string, price: number): Observable<Listing> {
@@ -81,10 +89,16 @@ export class ListingsService {
   }
 
   editListing(id: string, name: string, description: string, price: number): Observable<Listing> {
-    return this.http.post<Listing>(
-      `/api/listings/${id}`,
-      { name, description, price },
-      httpOptions,
-    );
+    return new Observable<Listing>(observer => {
+      this.auth.user.subscribe(user => {
+        user && user.getIdToken().then(token => {
+          return this.http.post<Listing>(
+            `/api/listings/${id}`,
+            { name, description, price },
+            httpOptionsWithAuthToken(token),
+          ).subscribe(() => observer.next());
+        })
+      })
+    })
   }
 }
